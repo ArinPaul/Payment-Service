a6a8d4239ad15a687af72de23430abd9492b5395
ArinPaul <arinpaul24081999@gmail.com>
2022-03-28 11:40:43 +0530

    sql naming changed

--- Table_Creation.sql
+++ Table_Creation.sql
@@ -5,20 +5,20 @@ USE PAYMENTSERVICE;
 CREATE TABLE loan_payment_schedule
 (
     id INT PRIMARY KEY,
-    loan_application_id BIGINT NOT NULL,
-    emi FLOAT(10),
+    loan_application_id VARCHAR(50) NOT NULL,
+    emi INT,
     due_date DATE,
     payment_status  VARCHAR(20),
-    due_amount FLOAT(10),
+    due_amount INT,
     principal_amount FLOAT(10),
-    intrest_amount FLOAT(10)
+    interest_amount FLOAT(10)
 );
 
 
 CREATE TABLE loan_payment
 (
     id INT PRIMARY KEY,
-    loan_application_id BIGINT NOT NULL,
+    loan_application_id VARCHAR(50) NOT NULL,
     paid_amount INT,  
     payment_utr_id VARCHAR(15),
     payment_mode VARCHAR(10),
@@ -30,10 +30,10 @@ CREATE TABLE loan_payment
 CREATE TABLE loan_disbursal
 (
     id INT PRIMARY KEY,
-    loan_application_id BIGINT NOT NULL,
+    loan_application_id VARCHAR(50) NOT NULL,
     partner_id INT,  
-    bank_account_number BIGINT,
-    amount DOUBLE PRECISION,
+    bank_account_number VARCHAR(50),
+    amount INT,
     disbursal_date TIMESTAMP, 
     disbursal_utr_id  VARCHAR(20) 
 );
@@ -41,7 +41,7 @@ CREATE TABLE loan_disbursal
 CREATE TABLE log_payment_service
 (
     id INT PRIMARY KEY,
-    loan_application_id BIGINT ,
+    loan_application_id VARCHAR(50) ,
     operation VARCHAR(10),
     operation_time TIMESTAMP,
     entity_name  VARCHAR(10)
