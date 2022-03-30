325a952af2c1ed0c2bd37f3634f12fdc21a9a722
ArinPaul <arinpaul24081999@gmail.com>
2022-03-30 13:50:56 +0530

    Integration Local

--- src/main/java/com/whizdm/payment_service/controller/Controller.java
+++ src/main/java/com/whizdm/payment_service/controller/Controller.java
@@ -7,14 +7,12 @@ import com.whizdm.payment_service.utils.APICaller.APICaller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 
 @RestController
@@ -119,16 +117,8 @@ public class Controller implements ControllerService {
             caller.postAPICall("",emiDetails.getLoan_id(),"CloseApplication");
         }
 
-
-
         return new ResponseEntity<String>("EMI Payment Successfully Completed",HttpStatus.OK);
     }
-
-
-
-
-
-
 
 }
 
